ae733960faa21e7bb28078a1f45bf18269ed87d4
Lucas <lucasemiliano619@gmail.com>
2023-07-04 23:15:14 -0300

    Atualização

--- casco-vivo/src/pages/index.jsx
+++ casco-vivo/src/pages/index.jsx
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
 import { Inter } from 'next/font/google'
 
 import Links from '../components/links'
